8ad91948476a881923544b020e8599663480bfb0
Mélissa Bouchard <pandora25_@hotmail.com>
2019-09-15 13:26:35 -0400

    Updates

---
+++ precache-manifest.f88a9636826893c84faa975ec046488c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3af409eaf631b4ad77f5d95c59d591f",
+    "url": "/images/index.html"
+  },
+  {
+    "revision": "a9737e99c54018fbf91d",
+    "url": "/images/static/css/main.c00f04db.chunk.css"
+  },
+  {
+    "revision": "dc4644de5faf670bd0fa",
+    "url": "/images/static/js/2.bf1553fb.chunk.js"
+  },
+  {
+    "revision": "a9737e99c54018fbf91d",
+    "url": "/images/static/js/main.92a6af1d.chunk.js"
+  },
+  {
+    "revision": "771799919abdadf6f752",
+    "url": "/images/static/js/runtime~main.6c16ec11.js"
+  }
+]);
